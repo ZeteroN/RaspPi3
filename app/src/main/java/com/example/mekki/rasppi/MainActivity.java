d9123ca43772b76a52b3b7772c3c0fa6936d379b
ZeteroN <mekki47b@hotmail.com>
2017-10-26 10:57:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mekki/rasppi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mekki.rasppi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void goToActivity2 (View view){
+        Intent intent = new Intent (this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
